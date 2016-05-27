aaf6b678f766c4a2e3744d191b38436d211a34d3
Belovic94 <victor.belosevich94@gmail.com>
2016-05-26 22:25:27 -0300

    Edito algunos metodos de la clase Cola_Reproduccion, falta revisar los metodos Rehacer modificaciones y Obtener n siguientes

--- cola_reproduccion.py
+++ cola_reproduccion.py
@@ -1,11 +1,13 @@
-import os
+EXTENSIONES_ACEPTADAS = ("wav", "mp3", "flac", "ogg", "wma")
+SUSTRACCION = '-'
+ADICION = '+' 
+# puse los import despues por que sino no funcionaba la llamada a SUSTRACCION en pilahistorial
 
+import os
 from cancion import Cancion
 from pilahistorial import PilaHistorial, Accion
 
-EXTENSIONES_ACEPTADAS = ("wav", "mp3", "flac", "ogg", "wma")
-SUSTRACCION='-'
-ADICION='+'
+
 
 class ColaDeReproduccion:
 	"""Clase que representa la cola de reproduccion del reproductor. Permite agregar y remover 
@@ -15,20 +17,23 @@ class ColaDeReproduccion:
 	def __init__(self, lista_canciones = []):
 		""" Recibe una lista de objetos de clase Cancion con las canciones que se quieren 
 		reproducir."""
-		self.canciones=lista_canciones
-		self.actual=0
-		self.historial=PilaHistorial()
+		self.canciones = lista_canciones
+		self.actual = 0   
+		self.historial = PilaHistorial()
+	
 	def cancion_actual(self):
 		""" Devuelve un objeto de clase Cancion que corresponde a la cancion actual, o None si no 
 		hay canciones cargadas en la cola."""
-		if self.canciones is []:
-			raise ValueError('La cola de reproducción está vacía')
+		if not self.canciones:
+			#raise ValueError('La cola de reproducción está vacía')
+			return None # tiraba error sino se ingresa ninguna ruta al llamar al programa, ahora te permite abrir 
+						#el reproducto sin ingresar ningun directorio con canciones
 		return self.canciones[self.actual]
 
 	def cancion_siguiente(self):
 		""" Devuelve un objeto de clase Cancion que corresponde a la cancion siguiente en la cola, 
 		o None si no hay mas canciones."""
-		if self.canciones[self.actual] == self.canciones[-1] or self.canciones is []:
+		if self.canciones[self.actual] == self.canciones[-1] or not self.canciones:
 			return None
 		self.actual += 1
 		return self.canciones[self.actual]
@@ -36,7 +41,7 @@ class ColaDeReproduccion:
 	def cancion_anterior(self):
 		""" Devuelve un objeto de clase Cancion que corresponde a la cancion anterior en la cola, 
 		o None si no hay canciones en la misma o la actual es la primera de la cola."""
-		if self.canciones[self.actual] == self.canciones[0] or self.canciones is []:
+		if self.canciones[self.actual] == self.canciones[0] or not self.canciones:
 			return None
 		self.actual -= 1
 		return self.canciones[self.actual]
@@ -46,10 +51,8 @@ class ColaDeReproduccion:
 		""" Agrega una Cancion a la cola a partir de su ruta. Devuelve True si se agrego 
 		correctamente, False en caso contrario. Esta accion puede deshacerse y rehacerse."""
 		if not os.path.exists(ruta_cancion):
-			#raise ValueError('La ruta/archivo no existe.')
 			return False
-		if not os.path.splitext(ruta_cancion)[1] in EXTENSIONES_ACEPTADAS:
-			#raise TypeError('El archivo no es compatible.')
+		if not os.path.splitext(ruta_cancion)[1][1:] in EXTENSIONES_ACEPTADAS: # te devolvia la extension con un punto, ahora se lo quité
 			return False
 		try:
 			self.canciones.append(Cancion(ruta_cancion))
@@ -59,12 +62,13 @@ class ColaDeReproduccion:
 			return True
 		except IOError:
 			return False
+	
 	def remover_cancion(self, ruta_cancion):
 		""" Remueve una Cancion de la cola a partir de su ruta. Devuelve True si se removio 
 		correctamente, False en caso contrario. Esta accion puede deshacerse y rehacerse."""
 		for x in range(len(self.canciones)):
-			if self.canciones[x] == ruta_cancion:
-				self.historial.apilar(Accion(self.canciones.pop(x)))
+			if self.canciones[x].obtener_ruta() == ruta_cancion:
+				self.historial.apilar((Accion(self.canciones.pop(x)), x))
 				# ---------re-ver---------------
 				return True
 		return False
@@ -72,11 +76,12 @@ class ColaDeReproduccion:
 	def deshacer_modificacion(self):
 		""" Deshace la ultima accion realizada. Devuelve True si pudo deshacerse, False en caso 
 		contrario."""
-		try:
-			if self.historial.ver_tope().tipo == ADICION:
-				self.canciones.append(self.historial.desapilar().cancion)
+		try: # Devuelve la cancion en la posicion en la que estaba en la lista
+			cancion, pos = self.historial.desapilar()
+			if cancion and cancion.tipo == ADICION:
+				self.canciones.insert(pos, cancion.cancion)				
 				return True
-		except: #pila esta vacia
+		except TypeError: #pila esta vacia
 			return False
 
 
@@ -86,19 +91,21 @@ class ColaDeReproduccion:
 
 		#incompleto
 		se_pudo=False
-		if self.historial.ver_tope().tipo == ADICION:
-			for cancion in self.canciones:
-				if cancion.ruta in self.historial.ver_tope().cancion.obtener_ruta():
-					se_pudo=True
-					self.canciones.remove(cancion)
-		else:
-			ultima_accion=self.historial.ver_tope()
-			while ultima_accion.cancion.obtener_ruta() in \
-					self.historial.ver_tope().cancion.obtener_ruta():
-				self.canciones.append(self.historial.desapilar())
-			se_pudo=True
-		return se_pudo
-
+		try:
+			if self.historial.ver_tope().tipo == ADICION:
+				for cancion in self.canciones:
+					if cancion.ruta in self.historial.ver_tope().cancion.obtener_ruta():
+						se_pudo=True
+						self.canciones.remove(cancion)
+			else:
+				ultima_accion=self.historial.ver_tope()
+				while ultima_accion.cancion.obtener_ruta() in \
+						self.historial.ver_tope().cancion.obtener_ruta():
+					self.canciones.append(self.historial.desapilar())
+				se_pudo=True
+			return se_pudo
+		except:
+			return False
 	def obtener_n_siguientes(self, n_canciones):
 		""" Devuelve una lista con las siguientes n canciones. Si en la cola de reproduccion 
 		quedan menos canciones que las pedidas, la lista contendra menos elementos que los 
